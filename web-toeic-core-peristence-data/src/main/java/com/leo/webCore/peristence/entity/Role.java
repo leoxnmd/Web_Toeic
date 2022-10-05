b40b16125f909cdb79986a77e4c77a11fa6c733f
leo <nmd1682001@gmail.com>
2022-10-05 14:33:39 +0700

    update bai 10-14

--- web-toeic-core-peristence-data/src/main/java/com/leo/webCore/peristence/entity/Role.java
+++ web-toeic-core-peristence-data/src/main/java/com/leo/webCore/peristence/entity/Role.java
@@ -1,4 +1,34 @@
 package com.leo.webCore.peristence.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @Column(name = "roleid")
+    private String roleId;
+    @Column(name = "name")
+    private String name;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- web-toeic-core-peristence-data/src/main/java/com/leo/webCore/peristence/entity/User.java
+++ web-toeic-core-peristence-data/src/main/java/com/leo/webCore/peristence/entity/User.java
@@ -1,4 +1,64 @@
 package com.leo.webCore.peristence.entity;
 
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity//class này là một entity(đối tượng, thực thể)
+@Table(name = "user")//object này tương ứng với bảng user trong sql
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "fullname")
+    private String fullName;
+    @Column(name = "createddate")
+    private Timestamp createdDate;
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
 }
